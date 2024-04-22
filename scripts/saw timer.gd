afc2c74dd7605d6487c68ec70e551fb647f81db6
cpalomares01 <cp8135482@gmail.com>
2024-04-22 12:35:39 -0700

    added pause menu

--- scripts/saw timer.gd
+++ scripts/saw timer.gd
@@ -5,18 +5,16 @@ var saw_heights = [60, 120, 180, 240, 300, 360, 420, 480, 550]
 
 func _ready():
 	wait_time = 1
-	#wait_time = randf_range(0,1)
 
 #spawns saws in random position on a set timer
 func _on_timeout():
 	# Randomly select an integer from myArray
 	var saw_height = saw_heights[randi() % saw_heights.size()]
-	#wait_time = randf_range(0,1)
 	randomize()
 	var next_obstacle = saw.instantiate()
 	next_obstacle.position = Vector2(-100, saw_height)
-	#next_obstacle.position = Vector2(-100, randi_range(0, 555))
 	add_child(next_obstacle)
+	
 	if wait_time > .02:
 		wait_time -= .01
 	else:
---
+++ scripts/score timer.gd
@@ -0,0 +1,16 @@
+extends Timer
+
+var score = 0
+var score_label: Label
+
+func _ready():
+	print(score)
+	score_label = $"../ScoreLabel"
+	update_score_label()
+
+func update_score_label():
+	score_label.text = "Score: " + str(score)
+
+func _on_timeout():
+	score += 1
+	update_score_label()
