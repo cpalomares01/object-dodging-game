c29a9bd4976923f2865d34af2b44118ec8cb09fc
cpalomares01 <cp8135482@gmail.com>
2024-04-22 10:40:49 -0700

    added jump(fix weird gliding when coming down)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
+#constants
 const MAX_SPEED = 300
 const ACCELERATION = 50
 const FRICTION = 20
-const JUMP_VELOCITY = -2000
-const GRAVITY = 5000
+const JUMP_VELOCITY = -1000
+const GRAVITY = 2500
 
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#variables
 var input = Vector2.ZERO
+var max_jumps = 2
+var jump_count = 0
 
 func _physics_process(delta):
 	var input_direction: Vector2 = get_input()
@@ -19,6 +22,7 @@ func _physics_process(delta):
 	player_movement(delta)
 	
 	velocity.y += GRAVITY * delta
+	handle_jumps()
 	player_movement(delta)
 
 func get_input() -> Vector2:
@@ -36,3 +40,13 @@ func decelerate():
 
 func player_movement(delta):
 	move_and_slide()
+
+func handle_jumps():
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			jump_count = 0
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
+		if not is_on_floor() and jump_count < max_jumps:
+			velocity.y = JUMP_VELOCITY
+			jump_count += 1
