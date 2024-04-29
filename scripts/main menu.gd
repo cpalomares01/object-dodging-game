5175c6ac0d0cc83e5d35a8d157b915eddb52b83b
cpalomares01 <cp8135482@gmail.com>
2024-04-28 21:11:57 -0700

    added coming soon screen to main menu buttons

--- scripts/main menu.gd
+++ scripts/main menu.gd
@@ -4,10 +4,10 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 func _on_options_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/coming soon.tscn")
 
 func _on_credits_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/coming soon.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
