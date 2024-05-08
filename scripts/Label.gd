5c982b8eb3490c10e116de64bc4c9c6b1c9f30c7
cpalomares01 <cp8135482@gmail.com>
2024-05-08 16:02:45 -0700

    i dont know why these are stuck in my terminal

--- scripts/Label.gd
+++ scripts/Label.gd
@@ -4,8 +4,8 @@ extends Label
 
 var high_score_label: Label
 
-func _ready():
-	high_score_label = $"../ScoreLabel"
+#func _ready():
+#	high_score_label = $"../ScoreLabel"
 
 func update_score_label():
 	#score = high_score
